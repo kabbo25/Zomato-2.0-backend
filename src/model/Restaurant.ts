3e2bd3011527d776a83389e65c905d9e61d0a6ed
kabbo25 <kabbo25@gmail.com>
2024-03-31 02:27:02 +0600

    created a restaurant schema

---
+++ src/model/Restaurant.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+
+const menuSchema = new mongoose.Schema({
+    ItemName: {type: String, required: true},
+    ItemPrice: {type: Number, required: true},
+})
+const restaurantSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    RestaurantName: {type: String, required: true},
+    City: {type: String, required: true},
+    Country: {type: String, required: true},
+    DeliveryTime: {type: Number, required: true},
+    DeliveryPrice: {type: Number, required: true},
+    Cuisine: [{type: String, required: true}],
+    MenuItems: [menuSchema],
+    ImageUrl: {type: String, required: true},
+    LastUpdated: {type: Date, required: true}
+})
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+export default Restaurant;

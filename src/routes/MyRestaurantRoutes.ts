eaf524b3a2b563adcfee5d0a767b9f49c48b6fca
kabbo25 <kabbo25@gmail.com>
2024-03-31 02:27:15 +0600

    created a restaurant route

---
+++ src/routes/MyRestaurantRoutes.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import multer from "multer";
+import MyRestaurantController from "../controllers/MyRestaurantController";
+
+const router=express.Router();
+
+const storage=multer.memoryStorage();
+const upload=multer({
+    storage:storage,
+    limits:{
+        fileSize:5*1024*1024 //5MB
+    }
+});
+
+router.post("/", upload.single("imageFile"), MyRestaurantController.createMyRestaurant);

e9606f15c137bf4b4e5548b98ca0d2f76d03cdeb
kabbo25 <kabbo25@gmail.com>
2024-03-31 01:36:20 +0600

    connect to cloudinary

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoutes from "./routes/MyUserRoutes";
-
+import {v2 as cloudinary} from "cloudinary";
 mongoose.connect(process.env.MONGODB_URI as string).then(() => {
     console.log("Connected to MongoDB");
+});
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 const app = express();
 app.use(express.json());

07eb694e6295c804d284b4517ea0b578cc05ded9
kabbo25 <kabbo25@gmail.com>
2024-05-10 16:04:38 +0600

    feat: implement put route to update the restaurant data

--- src/controllers/MyRestaurantController.ts
+++ src/controllers/MyRestaurantController.ts
@@ -1,43 +1,74 @@
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
 import Restaurant from "../model/Restaurant";
-import cloudinary from 'cloudinary';
+import cloudinary from "cloudinary";
 import mongoose from "mongoose";
 
-
 const getCurrentRestaurant = async (req: Request, res: Response) => {
-    try {
-        const curRestaurant = await Restaurant.findOne({User: req.userId});
-        if (!curRestaurant) {
-            res.status(404).send({message: "Restaurant not found"});
-            return;
-        }
-        res.json(curRestaurant);
-    } catch {
-        res.status(500).send({message: "Error fetching Restaurant data"});
+  try {
+    const curRestaurant = await Restaurant.findOne({ User: req.userId });
+    console.log(req.userId);
+    console.log(curRestaurant);
+    if (!curRestaurant) {
+      res.status(404).send({ message: "Restaurant not found" });
+      return;
     }
-}
+    res.json(curRestaurant);
+  } catch {
+    res.status(500).send({ message: "Error fetching Restaurant data" });
+  }
+};
 const createMyRestaurant = async (req: Request, res: Response) => {
-    try {
-        const currentRestaurant = await Restaurant.findOne({user: req.userId});
-        if (currentRestaurant) {
-            return res.status(409).send('restaurant already exists');
-        }
-        const imageFile = req.file as Express.Multer.File;
-        const base64Image = Buffer.from(imageFile.buffer).toString('base64');
-        const dataURI = `data:${imageFile.mimetype};base64,${base64Image}`;
-        const imageResponse = await cloudinary.v2.uploader.upload(dataURI);
-        const newRestaurant = new Restaurant(req.body);
-        newRestaurant.ImageUrl = imageResponse.url;
-        newRestaurant.User = new mongoose.Types.ObjectId(req.userId);
-        newRestaurant.LastUpdated = new Date();
-        await newRestaurant.save();
-        res.send(newRestaurant);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('failed to create restaurant');
+  try {
+    const currentRestaurant = await Restaurant.findOne({ User: req.userId });
+    if (currentRestaurant) {
+      return res.status(409).send("restaurant already exists");
     }
-}
+    const ImageUrl = await uploadImage(req.file as Express.Multer.File);
+    const newRestaurant = new Restaurant(req.body);
+    newRestaurant.ImageUrl = ImageUrl;
+    newRestaurant.User = new mongoose.Types.ObjectId(req.userId);
+    newRestaurant.LastUpdated = new Date();
+    await newRestaurant.save();
+    res.send(newRestaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("failed to create restaurant");
+  }
+};
+
+const updateMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const currentRestaurant = await Restaurant.findOne({ User: req.userId });
+    console.log(req.userId);
+    console.log(currentRestaurant);
+    if (!currentRestaurant) {
+      return res.status(404).send("restaurant not found");
+    }
+    const ImageUrl = await uploadImage(req.file as Express.Multer.File);
+    currentRestaurant.RestaurantName = req.body.RestaurantName;
+    currentRestaurant.City = req.body.City;
+    currentRestaurant.Country = req.body.Country;
+    currentRestaurant.DeliveryTime = req.body.DeliveryTime;
+    currentRestaurant.DeliveryPrice = req.body.DeliveryPrice;
+    currentRestaurant.Cuisine = req.body.Cuisine;
+    currentRestaurant.MenuItems = req.body.MenuItems;
+    currentRestaurant.ImageUrl = ImageUrl;
+    currentRestaurant.LastUpdated = new Date();
+    await currentRestaurant.save();
+    res.status(201).json(currentRestaurant);
+  } catch {
+    res.status(500).send({ message: "Error updating Restaurant data" });
+  }
+};
+
+const uploadImage = async (File: Express.Multer.File) => {
+  const base64Image = Buffer.from(File.buffer).toString("base64");
+  const dataURI = `data:${File.mimetype};base64,${base64Image}`;
+  const imageResponse = await cloudinary.v2.uploader.upload(dataURI);
+  return imageResponse.url;
+};
 export default {
-    getCurrentRestaurant,
-    createMyRestaurant
+  getCurrentRestaurant,
+  createMyRestaurant,
+  updateMyRestaurant,
 };
--- src/routes/MyRestaurantRoutes.ts
+++ src/routes/MyRestaurantRoutes.ts
@@ -1,20 +1,39 @@
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
-import {validateMyRestantRequest} from "../middlewares/validation";
-import {jwtCheck, jwtParse} from "../middlewares/auth";
+import { validateMyRestantRequest } from "../middlewares/validation";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
 
-const router=express.Router();
+const router = express.Router();
 
-const storage=multer.memoryStorage();
-const upload=multer({
-    storage:storage,
-    limits:{
-        fileSize:5*1024*1024 //5MB
-    }
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5MB
+  },
 });
 
-router.get("/",jwtCheck,jwtParse,MyRestaurantController.getCurrentRestaurant);
-router.post("/", upload.single("ImageFile"),validateMyRestantRequest,jwtCheck, jwtParse,MyRestaurantController.createMyRestaurant);
-
+router.get(
+  "/",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getCurrentRestaurant,
+);
+router.post(
+  "/",
+  upload.single("ImageFile"),
+  validateMyRestantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant,
+);
+router.put(
+  "/",
+  upload.single("ImageFile"),
+  validateMyRestantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant,
+);
 export default router;
